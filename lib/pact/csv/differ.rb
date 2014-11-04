184476f7b09bf551d658201607c02c154f2eff5f
Martin Mauch <mauch@crealytics.de>
2014-11-04 21:05:29 +0100

    WIP: Use daff for table diffing.

--- lib/pact/csv/differ.rb
+++ lib/pact/csv/differ.rb
@@ -1,4 +1,4 @@
-require 'cucumber'
+require 'daff'
 
 module Pact
   module CSV
@@ -21,17 +21,21 @@ module Pact
         actual_data = ::CSV.parse(actual, headers: true)
         compare(expected_data, actual_data)
       end
+
       def compare(expected, actual)
-        expected_table = Cucumber::Ast::Table.new(expected.map(&:to_hash))
-        actual_table = Cucumber::Ast::Table.new(actual.map(&:to_hash))
-        errors = []
-        begin
-          expected_table.diff!(actual_table)
-        rescue => e
-          errors = [e.table]
-        end
-        errors
+        aligned = Coopy::Coopy.compare_tables(to_table(expected), to_table(actual)).align
+        flags = Daff::CompareFlags.new
+        table_diff = Daff::SimpleTable.new(0,0)
+        highlighter = Daff::TableDiff.new(aligned, flags)
+        highlighter.hilite(table_diff)
+        [table_diff.data, table_diff.get_width, table_diff.get_height]
       end
+      def to_table(arr)
+        t = Daff::SimpleTable.new(arr.first.size, arr.size)
+        arr.each_with_index {|row, row_num| row.each_with_index {|cell, col_num| t.set_cell(col_num, row_num, cell)}}
+        t
+      end
+
     end
   end
 end
